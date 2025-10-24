dd3131c5690f8c95aac103cc0a43182efced2d2a
yash bahuguna <yashbahuguna918@gmail.com>
2025-10-24 17:31:56 +0530

    2

--- js/index.js
+++ js/index.js
@@ -10,4 +10,35 @@ const[first,second,...rest]= t
 
 console.log(first)
 console.log(second)
-console.log(rest)
+
+const obj1 ={
+    name: 'alice',
+    age: 25,
+    education:'bachelor',
+
+}
+const obj2 ={
+    name: 'arto hellas',
+    agee: 34
+}
+
+const object3 ={
+    name:{
+        first:'dan',
+        last:'smith',  
+    },
+    grades:[2,3,4,5,],
+    department:'computer science'
+
+}
+console.log(obj1.name)
+
+
+const sum = (p1,p2) =>{
+    console.log(p2)
+    console.log(p1)
+    return p1*p2
+}
+
+const result = sum(3,4)
+console.log(result)
